bbfa2ac42f31a8bad985565021a56655d172e55f
shoaib-asim17 <shoaibasim1990@gmail.com>
2024-01-11 23:33:11 +0530

    pattern 2

---
+++ Pattern2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pattern2 {
+    
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the pattern");
+        Scanner in= new Scanner(System.in);
+        int size=in.nextInt();
+ 
+        patternfuction(size);
+ 
+    }
+     static void patternfuction(int size)
+     {
+         int row,col;
+ 
+         for(row=1;row<=size;row++)
+         {
+             for(col=1;col<=size;col++)
+             {
+                 System.out.print("* ");
+             }
+             System.out.println("");
+         }
+     }
+    
+}
